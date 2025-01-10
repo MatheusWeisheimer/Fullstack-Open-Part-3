b81c9960bf989249d1f013ff6a2478b82be6438f
Matheus <matheusweish@gmail.com>
2025-01-10 17:54:38 -0300

    3.2 done

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const persons = require('./persons.json')
 
 const app = express()
+const date = new Date()
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date.toString()}</p>
+    `)
+})
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
