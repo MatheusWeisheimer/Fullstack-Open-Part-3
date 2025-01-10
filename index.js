dfeae7639926af746fe627cddf5bddce4ec1949f
Matheus <matheusweish@gmail.com>
2025-01-10 17:44:22 -0300

    3.1 done

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const persons = require('./persons.json')
+
+const app = express()
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
